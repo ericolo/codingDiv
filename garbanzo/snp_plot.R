029a61c554a5301dcc04ebd2c8f239ae2a51704f
ericolo <52046230+ericolo@users.noreply.github.com>
2022-12-02 14:00:18 +0100

    Update snp_plot.R

--- garbanzo/snp_plot.R
+++ garbanzo/snp_plot.R
@@ -32,7 +32,7 @@ if (size<=20000) {
   window_size=15
   text_size=13
   } else if (size<=50000) {
-    plot_size=18.1
+    plot_size=17.8
     window_size=50
     text_size=13
     } else if (size>50000) {
