1d8afb91156947d5f789583a78c81fa246cec363
ericolo <eric.olo.ndela@outlook.com>
2023-01-04 09:30:22 +0100

    Update snp_plot.R

--- scripts/snp_plot.R
+++ scripts/snp_plot.R
@@ -32,7 +32,7 @@ if (size<=20000) {
   window_size=15
   text_size=12
   } else if (size<=50000) {
-    plot_size=17.1
+    plot_size=18.1
     window_size=50
     text_size=12
     } else if (size>50000) {
